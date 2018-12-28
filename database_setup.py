e97a63cce5280ac3aaf202042dfbfbec30a49ac2
pynoor <32771762+pynoor@users.noreply.github.com>
2018-12-28 12:34:09 +0100

    set up database

---
+++ database_setup.py
@@ -0,0 +1,61 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+
+    __tablename__ = 'category'
+
+    name = Column(
+        String(20), nullable=False
+    )
+
+    id = Column(
+        Integer, primary_key=True
+    )
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name
+        }
+
+class Item(Base):
+
+    __tablename__ = 'item'
+
+    name = Column(
+        String(20), nullable=False
+    )
+
+    description = Column(
+        String(500), nullable=True
+    )
+
+    category = Column(
+        Integer, ForeignKey('category.id')
+    )
+
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'category': self.category,
+        }
+
+engine = crearte_engine(
+    'sqlite:///catalog.db'
+)
+
+Base.metadata.create_all(engine)
