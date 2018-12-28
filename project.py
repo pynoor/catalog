5ad44cae33ee2afbc0363a4650a3238861ba9cea
pynoor <32771762+pynoor@users.noreply.github.com>
2018-12-28 13:25:37 +0100

    Add homepage

--- project.py
+++ project.py
@@ -1,6 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Item, Category
+
+
+engine = create_engine('sqlite:///restaurantmenu.db',
+connect_args={'check_same_thread': False})
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    return "Welcome"
+
 if __name__ == '__main__':
     app.secret_key = '17_no_0)(31'
     app.debug = True
