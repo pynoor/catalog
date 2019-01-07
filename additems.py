fd0f67f155d7c9311b02caf4f7a709abb06867b7
pynoor <32771762+pynoor@users.noreply.github.com>
2019-01-07 15:18:18 +0100

    Add html for flashed messages

---
+++ additems.py
@@ -0,0 +1,130 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Category, Item, Base
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+rihanna = Item(
+    name='Rihanna',
+    description='',
+    category_id=1
+    )
+session.add(rihanna)
+session.commit()
+
+muddy_waters = Item(
+    name='Muddy Waters',
+    description='',
+    category_id=2
+    )
+session.add(muddy_waters)
+session.commit()
+
+beyonce = Item(
+    name='Beyonce',
+    description='',
+    category_id=3
+    )
+session.add(beyonce)
+session.commit()
+
+erykah_badu = Item(
+    name='Erykah Badu',
+    description='Blablabla',
+    category_id=4
+    )
+session.add(erykah_badu)
+session.commit()
+
+jill_scott = Item(
+    name='Jill Scott',
+    description='',
+    category_id=4
+    )
+session.add(jill_scott)
+session.commit()
+
+india_arie = Item(
+    name='India Arie',
+    description='',
+    category_id=4
+    )
+session.add(india_arie)
+session.commit()
+
+dangelo = Item(
+    name="D'Angelo",
+    description='',
+    category_id=4
+    )
+session.add(dangelo)
+session.commit()
+
+richard_bona = Item(
+    name='Richard Bona',
+    description='',
+    category_id=5
+    )
+session.add(richard_bona)
+session.commit()
+
+micheal_jackson = Item(
+    name='Micheal Jackson',
+    description='',
+    category_id=6
+    )
+session.add(micheal_jackson)
+session.commit()
+
+
+fela_kuti = Item(
+    name='Fela Kuti',
+    description='',
+    category_id=7
+    )
+session.add(fela_kuti)
+session.commit()
+
+chronix = Item(
+    name='Chronix',
+    description='',
+    category_id=8
+    )
+session.add(chronix)
+session.commit()
+
+busy_signal = Item(
+    name='Busy Signal',
+    description='',
+    category_id=9
+    )
+session.add(busy_signal)
+session.commit()
+
+lyrikal = Item(
+    name='Lyrikal',
+    description='',
+    category_id=10
+    )
+session.add(lyrikal)
+session.commit()
+
+tony_vega = Item(
+    name='Tony Vega',
+    description='',
+    category_id=11
+    )
+session.add(tony_vega)
+session.commit()
+
+xtreme = Item(
+    name='Xtreme',
+    description='',
+    category_id=12
+    )
+session.add(xtreme)
+session.commit()
---
+++ dbscript.py
@@ -0,0 +1,58 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Category, Item, Base
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+r_n_b = Category(name='rnb')
+session.add(r_n_b)
+session.commit()
+
+
+blues = Category(name='blues')
+session.add(blues)
+session.commit()
+
+hip_hop = Category(name='hiphop')
+session.add(hip_hop)
+session.commit()
+
+soul = Category(name='soul')
+session.add(soul)
+session.commit()
+
+jazz = Category(name='jazz')
+session.add(jazz)
+session.commit()
+
+pop = Category(name='pop')
+session.add(pop)
+session.commit()
+
+afrobeat = Category(name='afrobeat')
+session.add(afrobeat)
+session.commit()
+
+reggae = Category(name='reggae')
+session.add(reggae)
+session.commit()
+
+dancehall = Category(name='dancehall')
+session.add(dancehall)
+session.commit()
+
+soca = Category(name='soca')
+session.add(soca)
+session.commit()
+
+salsa = Category(name='salsa')
+session.add(salsa)
+session.commit()
+
+bachata = Category(name='bachata')
+session.add(bachata)
+session.commit()
