f80cc61ae5c7d3e2fcec150f8a8d86afcc28b3c5
pynoor <32771762+pynoor@users.noreply.github.com>
2019-01-07 23:29:46 +0100

    bug: fix serializer

--- database_setup.py
+++ database_setup.py
@@ -1,14 +1,31 @@
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String
-
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import relationship
 
-from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    name = Column(
+        String(20), nullable=False
+    )
+
+    email = Column(
+        String(50), nullable=False
+    )
+
+    picture = Column(
+        String(40), nullable=True
+    )
+
+    id = Column(
+        Integer, primary_key=True
+    )
+
 
 class Category(Base):
 
@@ -44,22 +61,30 @@ class Item(Base):
         String(500), nullable=True
     )
 
+
     category_id = Column(
         Integer, ForeignKey('category.id')
     )
 
     category = relationship(Category)
 
+    user_id = Column(
+        Integer, ForeignKey('user.id')
+    )
+
+    user = relationship(User)
+
     @property
     def serialize(self):
         return {
             'name': self.name,
             'description': self.description,
-            'category': self.category,
+            'category': self.category_id,
+            'user': self.user_id
         }
 
 engine = create_engine(
-    'sqlite:///catalog.db'
+    'sqlite:///catalogwithusers.db'
 )
 
 Base.metadata.create_all(engine)
